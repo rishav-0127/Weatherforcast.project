c797ad27a2ae99a5abbc8c211a3253ad9daba5f3
rishav-0127 <rajputrishav0127@gmail.com>
2024-07-20 14:52:47 +0530

    first change

---
+++ new.js
@@ -0,0 +1,14 @@
+
+// console.log(45)
+// function sum(a,b,c){
+//     return a+b+c
+// }
+// result=sum(3,5,8)
+//  console.log(result)
+//  let arr =(1,2,3,4,5,6);
+//  let fact=(a,b)=>{ 
+//     return a*b;
+//  }
+//  console.log(arr.reduce(fact))
+console.log("harry")
+
